7a8dbf6e9156f11aeffed24dd7357cc9d185e588
Mirangga Jakti <158492350+RanggaRvc@users.noreply.github.com>
2025-12-31 02:26:34 +0700

    add input handling example

---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+
+if age >= 18:
+    print(f"Hello {name}, you are an adult.")
+else:
+    print(f"Hello {name}, you are under 18.")

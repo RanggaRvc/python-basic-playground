adce3004cd13e66bd07f2155309b10d9ac58b2f1
Mirangga Jakti <158492350+RanggaRvc@users.noreply.github.com>
2025-12-31 02:51:19 +0700

    fix function call syntax in function.py

--- function.py
+++ function.py
@@ -1,5 +1,23 @@
 def greet(name):
   return f"Hello, {name}!"
 
-message = greet{"Rangga"}
+message = greet("Rangga")
 print(message)
+
+
+def get_student_info(student: dict) -> str:
+    return (
+      f"Name: {student['name']}\n"
+      f"Age: {student['age']}\n"
+      f"Major: {student['major']}\n"
+    )
+
+
+student_data = {
+  "name": "Rangga",
+  "age": 21,
+  "major": "Computer Science"
+}
+
+result = get_student_info(student_data)
+print(result)

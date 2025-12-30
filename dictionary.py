14a13305805af3c49c96fa0838118f898303953f
Mirangga Jakti <158492350+RanggaRvc@users.noreply.github.com>
2025-12-31 02:38:39 +0700

    add basic dictionary usage example

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "nama": "Rangga",
+    "age": 21,
+    "major": "Computer Science"
+}
+
+print("Students Information")
+print(f"Name: {student['name']}")
+print(f"Age: {student['age']}")
+print(f"Major: {student['major']}")
